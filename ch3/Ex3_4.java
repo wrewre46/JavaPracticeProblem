748ab0e7a396165f6cb3e2463136ce62578ee457
Jinwoo, Nam <66236978+wrewre46@users.noreply.github.com>
2022-03-29 23:14:37 +0900

    update

---
+++ ch3/Ex3_4.java
@@ -0,0 +1,8 @@
+package ch3;
+
+public class Ex3_4 {
+    public static void main(String[] args) {
+        int num=456;
+        System.out.println((num/100)*100); //100의 자리 이하를 버리는 코드.
+    }
+}
---
+++ ch3/Ex3_5.java
@@ -0,0 +1,8 @@
+package ch3;
+
+public class Ex3_5 {
+    public static void main(String[] args) {
+        int num=777;
+        System.out.println((num/10)*10+1); // 일의 자리를 1로 바꾸는 코드
+    }
+}
