4b55613285d41c2b7368b5901a1d4d5dd45c8018
Jinwoo, Nam <66236978+wrewre46@users.noreply.github.com>
2022-04-08 23:33:10 +0900

    update

---
+++ ch4/Ex4_5.java
@@ -0,0 +1,15 @@
+package ch4;
+class Ex4_5{
+    public static void main(String[] args) {
+        int i=0;
+        while(i<=10){
+            int j =0;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
