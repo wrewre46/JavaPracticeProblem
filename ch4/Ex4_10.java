78b595dbd5f033c7c92e81c48d4295d57fb21b64
Jinwoo, Nam <66236978+wrewre46@users.noreply.github.com>
2022-04-12 23:08:18 +0900

    update

---
+++ ch4/Ex4_10.java
@@ -0,0 +1,14 @@
+package ch4;
+
+public class Ex4_10 {
+    public static void main(String[] args) {
+        int num=12345;
+        int sum=0;
+        for(int i =1; i<=5;i++){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        System.out.println(sum);
+    }
+    
+}
