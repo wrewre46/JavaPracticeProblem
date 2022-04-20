6fd647d6ea1365a85e16e7ba2af2f8f5c65a3bb6
Jinwoo, Nam <66236978+wrewre46@users.noreply.github.com>
2022-04-20 23:38:09 +0900

    update

---
+++ ch5/Ex5_2.java
@@ -0,0 +1,12 @@
+package ch5;
+public class Ex5_2 {
+    public static void main(String[] args) {
+        int[][] arr = {
+            { 5, 5, 5, 5, 5},
+            { 10, 10, 10},
+            { 20, 20, 20, 20},
+            { 30, 30}
+            };
+        System.out.println(arr.length);
+    }
+}
